6c9c3e607fc7758df550ef48e16a4ba6a65648b1
wisdom5565 <wisdom5565@naver.com>
2022-12-20 15:37:43 +0900

    식당 관리자 Spring 프로젝트

---
+++ src/main/java/com/catchmind/resadmin/ResAdminApplication.java
@@ -0,0 +1,13 @@
+package com.catchmind.resadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResAdminApplication.class, args);
+    }
+
+}
